214bc37e5f0779331548185eb8580a14c45a7d9a
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-07-23 20:15:43 -0400

    Completed model class

---
+++ src/main/java/com/lendingcatalog/model/Movie.java
@@ -0,0 +1,83 @@
+package com.lendingcatalog.model;
+
+import com.lendingcatalog.util.FileStorageService;
+import com.lendingcatalog.util.exception.FileStorageException;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class Movie implements CatalogItem {
+
+    private String id;
+    private String name;
+    private String director;
+    private LocalDate releaseDate;
+
+
+    public Movie(String name, String director, LocalDate releaseDate) {
+        this.name = name;
+        this.director = director;
+        this.releaseDate = releaseDate;
+    }
+
+    public Movie(String name, String director, String releaseDate) {
+        this(name, director, LocalDate.parse(releaseDate));
+    }
+
+    @Override
+    public boolean matchesName(String searchStr) {
+        return this.name.equalsIgnoreCase(searchStr);
+    }
+
+    @Override
+    public boolean matchesCreator(String searchStr) {
+        return this.director.equalsIgnoreCase(searchStr);
+    }
+
+    @Override
+    public boolean matchesYear(int searchYear) {
+        return releaseDate.getYear() == searchYear;
+    }
+
+    @Override
+    public void registerItem() {
+        this.id = UUID.randomUUID().toString();
+        try {
+            FileStorageService.writeContentsToFile(LocalDate.now().toString() + System.lineSeparator() + toString() + System.lineSeparator(), "src/main/logs/movie.txt", true);
+        } catch (FileStorageException e) {
+            e.getMessage();
+        }
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String toString() {
+        return "* MOVIE" + System.lineSeparator() + name + System.lineSeparator()
+                + " - Directed By: " + director + System.lineSeparator()
+                + " - Released On: " + releaseDate + System.lineSeparator()
+                + " - ID: " + id;
+    }
+}
---
+++ src/main/java/com/lendingcatalog/model/Tool.java
@@ -0,0 +1,58 @@
+package com.lendingcatalog.model;
+
+import com.lendingcatalog.util.FileStorageService;
+import com.lendingcatalog.util.exception.FileStorageException;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class Tool implements CatalogItem{
+
+    private String id;
+    private String type;
+    private String manufacturer;
+    int count;
+
+
+    public Tool(String type, String manufacturer, int count) {
+        this.type = type;
+        this.manufacturer = manufacturer;
+        this.count = count;
+    }
+
+    public Tool(String type, String manufacturer, String count) {
+        this(type, manufacturer, Integer.parseInt(count));
+    }
+
+    @Override
+    public boolean matchesName(String searchStr) {
+        return this.type.contains(searchStr.toLowerCase());
+    }
+
+    @Override
+    public boolean matchesCreator(String searchStr) {
+        return this.manufacturer.contains(searchStr.toLowerCase());
+    }
+
+    @Override
+    public boolean matchesYear(int searchYear) {
+        return false;
+    }
+
+    @Override
+    public void registerItem() {
+        this.id = UUID.randomUUID().toString();
+        try {
+            FileStorageService.writeContentsToFile(LocalDate.now().toString() + System.lineSeparator() + toString() + System.lineSeparator(), "src/main/resources/logs/tool.txt", true);
+        } catch (FileStorageException e) {
+            e.getMessage();
+        }
+    }
+
+    public String toString() {
+        return "* TOOL" + System.lineSeparator() + type + System.lineSeparator()
+                + " - Manufactured By: " + manufacturer + System.lineSeparator()
+                + " - Owns: " + count + System.lineSeparator()
+                + " - ID: " + id;
+    }
+}
